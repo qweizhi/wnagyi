64c0a7425a8093ce2403c0b20da00f334fdaf0af
wwf <1466228266>
2019-07-14 19:08:18 +0800

    123

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,46 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import home from './views/Index.vue';
+import search from './views/Search.vue';
+
 
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/home',
+            name: 'home',
+            component: home,
+        },
+        {
+            path: '/search',
+            name: 'search',
+            component: search,
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            // component: () => import(/* webpackChunkName: "about" */ './views/Search.vue'),
+        }, {
+            path: '/login',
+            name: 'login',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/Login.vue'),
+        }, {
+            path: '/my',
+            name: 'my',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/My.vue'),
+        }, {
+            path: '/',
+            redirect: '/home',
+            component: home,
+        },
+    ],
 });
